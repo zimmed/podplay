468942f7dc6360149928b0792925b55d6f43b03a
Ian McGaunn <ian_mcgaunn@student.uml.edu>
2015-02-19 12:36:19 -0500

    [build] added task to run server

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var less   = require('gulp-less');
 var path   = require('path');
+var app    = require('./app');
+var debug  = require('debug')('podplay:server');
+var http   = require('http');
+
 
 // compile less stylesheets
 gulp.task('style', function() {
@@ -19,8 +23,47 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter('default')); // maybe install a better reporter
 });
 
+// run the express server
+gulp.task('serve', function() {
+  var port = '8080';
+  app.set('port', port);
+
+  var server = http.createServer(app);
+
+  server.listen(port);
+  console.log('server running on port ' + port);
+  server.on('error', function(error) {
+
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  });
+
+  server.on('listening', function() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+  });
+
+})
+
 // build less, lint JS and run application
-gulp.task('default', ['style', 'lint'], function() {
-  console.log('this should run after style and lint');
+gulp.task('default', ['style', 'lint', 'serve'], function() {
+  console.log('all done');
 });
 
