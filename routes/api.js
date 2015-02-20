df5fe07085cddd384b13316605e053621d408ba4
zimmed <david.zimmelman@qfxgaming.com>
2015-02-20 12:35:01 -0500

    Added browse to api

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,38 @@ var request = require('request');
 var router = express.Router();
 
 router.get('/search', function(req, res, next) {
-     var api = 'https://itunes.apple.com/search?entity=podcast&term=';
-     var queryURL = api + req.query.name;
+    var api = 'https://itunes.apple.com/search?entity=podcast&term=';
+    // Name is ambiguous, and might confuse things when we implement local searching by station/artist/etc.
+    // For now, it's best to use `term` to avoid confusion.
+    var queryURL = api + req.query.term; 
 
-     console.log('url: ' + queryURL);
-     console.log(req.body);
+    console.log('url: ' + queryURL);
+    console.log(req.body);
 
-     request(queryURL, function(error, response, body) {
+    request(queryURL, function(error, response, body) {
         res.send(body);
-     });
+    });
+});
+
+router.get('/browse', function(req, res, next) {
+    var api = 'https://itunes.apple.com/us/rss/toppodcasts';
+    var limit = (req.query.limit)
+        ? '/limit=' + req.query.limit
+        : '/limit=200';
+    var explicit = (req.query.safe)
+        ? '/explicit=false'
+        : '/explicit=true';
+    var genre = (req.query.genre)
+        ? '/genre=' + req.query.genre
+        : '';
+    var queryURL = api + limit + genre + explicit + '/json';
+    
+    console.log('url: ' + queryURL);
+    console.log(req.body);
+    
+    request(queryURL, function(error, response, body) {
+        res.send(body);
+    });
 });
 
 module.exports = router;
