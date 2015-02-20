80d5375c183d6b2ec3a481cfd2eef51f82a96668
zimmed <david.zimmelman@qfxgaming.com>
2015-02-20 13:37:32 -0500

    Modified table population for alternative data.
    iTunes request for top charts retrieves data in different format.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,20 @@
 (function (window, $) {
     'use strict';
 
-    var populateTable = function (results) {
-        var podcast, row;
-        $('#results-table tbody > tr').remove();
-        for (podcast in results) {
-            row = $('<tr>');
-            row.append($('<td>').text(results[podcast].collectionName));
-            row.append($('<td>').text(results[podcast].feedUrl));
-            row.append($('<td>').text(results[podcast].primaryGenreName));
-            $('#results-table').append(row);
-        }
-    };
-
     $().ready(function () {
         $('#podcast-search').submit(function () {
             var searchTerm = $('#podcast-search-input').val();
 
             $.get('/api/search/?term=' + searchTerm, function (data) {
-                var results = JSON.parse(data).results;
-                populateTable(results);
+                var results = JSON.parse(data).results, podcast, row;
+                $('#results-table tbody > tr').remove();
+                for (podcast in results) {
+                    row = $('<tr>');
+                    row.append($('<td>').text(results[podcast].collectionName));
+                    row.append($('<td>').text(results[podcast].feedUrl));
+                    row.append($('<td>').text(results[podcast].primaryGenreName));
+                    $('#results-table').append(row);
+                }
             });
         });
 
@@ -27,8 +22,15 @@
             var genre = $('#podcast-browse-cat').val();
 
             $.get('/api/browse/?limit=10&genre=' + genre, function (data) {
-                var results = JSON.parse(data).results;
-                populateTable(results);
+                var results = JSON.parse(data).entry;
+                $('#results-table tbody > tr').remove();
+                for (podcast in results) {
+                    row = $('<tr>');
+                    row.append($('<td>').text(results[podcast]['im:name'].label));
+                    row.append($('<td>').text('N/A'));
+                    row.append($('<td>').text(results[podcast].category.attributes.label));
+                    $('#results-table').append(row);
+                }
             });
         });
     });
