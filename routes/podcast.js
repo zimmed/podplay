8b983016948728b3abbe528b3fccec4233d3b59e
zimmed <david.zimmelman@qfxgaming.com>
2015-02-24 10:32:42 -0500

    [Fix] Extra cases around missing feed data.
    Unbelievable.. some episodes have no audio data. Sigh.

--- routes/podcast.js
+++ routes/podcast.js
@@ -24,10 +24,12 @@ router.get('/:id/:title?', function (req, res, next) {
         request(feed, function (error, response, body) {
             parseString(body, function (err, obj) {
                 obj.rss.channel.genre = result.primaryGenreName;
+                if (!obj.rss.channel.genre) obj.rss.channel.genre = "N/A";
                 obj.rss.channel.imgs = { i600 : result.artworkUrl600,
                                                i100 : result.artworkUrl100,
                                                i60 : result.artworkUrl60,
                                                i30 : result.artworkUrl30 };
+                
                 res.render('podcast', { id: id, title: title, javascripts: ['podcast'], safetitle: safeTitle(title), feed: obj.rss.channel });
             });
         });
