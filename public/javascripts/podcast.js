571b60f059beb238d55fe3170f32d97bee01608c
zimmed <david.zimmelman@qfxgaming.com>
2015-02-23 20:23:13 -0500

    Added some styling and bugfix.

--- public/javascripts/podcast.js
+++ public/javascripts/podcast.js
@@ -2,6 +2,7 @@
     'use strict';
     
     $().ready(function () {
+        console.log('safetitling');
         var t = $('#safetitle').html();
         window.history.replaceState({}, document.title, '/' + t);
         $('#safetitle').remove();
