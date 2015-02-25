70f5d946b68c05ea48553bc6b4aeb5a83ae469a5
Ian McGaunn <ian_mcgaunn@student.uml.edu>
2015-02-25 17:37:48 -0500

    added basic audio player

--- public/javascripts/podcast.js
+++ public/javascripts/podcast.js
@@ -1,10 +1,32 @@
 (function (document, window, $) {
     'use strict';
-    
+
     $().ready(function () {
         var t = $('#safetitle').html();
         window.history.replaceState({}, document.title, '/podcast/' + t);
         $('#safetitle').remove();
+
+        // setup audio player
+        // by default use the first URL in the table
+        var audioURL = $('#episode-table .listenlink:first-child').attr('data-audio');
+        var player = $('<audio>');
+        player.addClass('podcast-player');
+        player.attr('src', audioURL);
+        player.attr('controls', 'controls');
+        $('#right-col').append(player);
+
+        // load new URL when user clicks on new podcast link
+        $('.listenlink').click(function() {
+            var audioURL = $(this).attr('data-audio');
+            player = $('audio');
+
+            player.attr('src', audioURL);
+            player[0].pause();
+            player[0].load();
+            player[0].play();
+
+        });
+
     });
 
 }(document, window, jQuery));
