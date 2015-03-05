bb96531f388e2d126a52072d158c6e721ca869c9
Ian McGaunn <ian_mcgaunn@student.uml.edu>
2015-03-05 11:52:40 -0500

    added audio player and associated styles

--- public/javascripts/podcast.js
+++ public/javascripts/podcast.js
@@ -8,17 +8,20 @@
 
         // setup audio player
         // by default use the first URL in the table
-        var audioURL = $('#episode-table .listenlink:first-child').attr('data-audio');
-        var player = $('<audio>');
-        player.addClass('podcast-player');
-        player.attr('src', audioURL);
-        player.attr('controls', 'controls');
-        $('#right-col').append(player);
+        // var audioURL = $('#episode-table .listenlink:first-child').attr('data-audio');
+        // var player = $('<audio>');
+        //  .addClass('podcast-player');
+        // player.attr('src', audioURL);
+        // player.attr('controls', 'controls');
+        // $('.container').append(player);
 
         // load new URL when user clicks on new podcast link
         $('.listenlink').click(function() {
             var audioURL = $(this).attr('data-audio');
-            player = $('audio');
+            var title    = $(this).attr('data-title');
+
+            $('.playing-title h3').html(title);
+            var player = $('audio');
 
             player.attr('src', audioURL);
             player[0].pause();
