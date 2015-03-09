d6fbc92e3cf633318a098c2d2bdb5600753d49e7
zimmed <david.zimmelman@qfxgaming.com>
2015-03-09 13:58:25 -0400

    debugging

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,12 @@
 /** 
  * Currenty unimplemented.
- *
+ */
 var express = require('express');
 var router = express.Router();
 
 // GET users listing.
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 module.exports = router;
- */
