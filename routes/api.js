40569a53c1f876d110416d1a2203c5a4bfdc08cd
zimmed <david.zimmelman@qfxgaming.com>
2015-04-18 12:32:15 -0400

    [Fix] Mongoose schema methods not working.

--- routes/api.js
+++ routes/api.js
@@ -35,13 +35,11 @@ router.get('/view/podcast/:id', function (req, res, next) {
     var id = req.params.id;
     // HTTP request information on podcast ID via Apple API
     Podcasts.getPodcast(id, function (error, msg) {
-        console.log("podcast view failure");
         res.render('error', { message: msg, error: error});
     }, function (podcast) {
         request(podcast.feedUrl, function (error, response, body) {
             // Data returned in XML format, and must be parsed.
             parseString(body, function (err, obj) {
-                console.log("podcast view success");
                 console.log(req.session.user);
                 // Render client 'podcast' page/view, passing necessary data.
                 res.render('podcast', {id: id,
