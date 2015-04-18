6a5035d83ddb521575eef1d89b66c2eb9fb84668
zimmed <david.zimmelman@qfxgaming.com>
2015-04-18 14:55:09 -0400

    [Debug] more defavorite

--- routes/users.js
+++ routes/users.js
@@ -201,6 +201,8 @@ router.get('/defavorite/:id', function (req, res, next) {
         users.delSubscription(req.session.user, pid, function (error, message) {
                 // Failure; error and message passed back.
                 if (error.err) console.log(error.err);
+                console.log('after fail: ' + JSON.stringify(error));
+                console.log(req.session.user.subscriptions);
                 res.json({message: message,
                           error: error,
                           status: error.status});
@@ -208,7 +210,7 @@ router.get('/defavorite/:id', function (req, res, next) {
             function (user) {
                 // Success; Update session user.
                 req.session.user = user;
-                console.log('after');
+                console.log('after success');
                 console.log(req.session.user.subscriptions);
                 res.json({message: 'Removed ' + pid + ' from favorites.',
                           status: 200}); // HTTP/1.1 200: OK
