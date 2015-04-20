1ea8ebbc4ffdf1384a61452e6fa0f6260b9d032a
zimmed <david.zimmelman@qfxgaming.com>
2015-04-20 15:48:23 -0400

    Added style to View All genre buttons

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -559,8 +559,10 @@
                     num += data.podcasts.length;
                 }
                 el.append('<div class="castnail btn-viewgenre" ' +
-                                'onclick="window.browseGenre(\'' + gid +
-                                '\');">View All</div>');
+                          'onclick="window.browseGenre(\'' + gid +
+                          '\');"><span class="glyphicon glyphicon' +
+                          '-triangle-bottom" aria-hidden="true">' +
+                          '</span><span>View All</span></div>');
                 window.shrink_panel(el, num);
             });
     };
