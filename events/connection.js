5e4361ed61dba9de80e2bbee37f40d73df5460b4
zimmed <david.zimmelman@qfxgaming.com>
2015-04-29 17:12:23 -0400

    [Fix] session

--- events/connection.js
+++ events/connection.js
@@ -2,8 +2,9 @@ var router = require('../lib/socket').Router();
 
 // connection event with no data passed
 router.add(function () {
-    var ready = true, s = this.handshake.session;
-    s.sockid = this.id;
+    var ready = true, s = this.request.session;
+    this.request.session.sockid = this.id;
+    s.bad_sockid = 100;
     if (s.user) {
         console.log(s.user.name + ' connected.');
         if (s.user.openSocket !== this.id) {
