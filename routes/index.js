79e7ef6dbe1d0a09f13c64e2b4b6eeb30c77cf19
zimmed <david.zimmelman@qfxgaming.com>
2015-04-29 16:54:24 -0400

    [Debug] ...

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,7 @@ router.get('/register', function (req, res, next) {
 });
 
 router.get('/sessiondump', function (req, res, next) {
+    console.log(req.session.id);
     console.log(JSON.stringify(req.session.user));
     console.log(JSON.stringify(req.session.playlist));
     res.json("Fuck off.");
