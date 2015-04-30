6cd32d14b074f6ce19d2431d24a929bd69a7ef87
zimmed <david.zimmelman@qfxgaming.com>
2015-04-30 09:25:50 -0400

    [Master] Port = 80

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('lint', function() {
 
 // run the express server
 gulp.task('serve', function() {
-  var port = '8080';
+  var port = '80';
   app.set('port', port);
 
   var server = http.createServer(app);
